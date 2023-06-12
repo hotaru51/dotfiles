bbd5298140383120bfe7a4062c0ac4bbf5719fea
hotaru <36354847+hotaru51@users.noreply.github.com>
2023-06-12 23:47:36 +0900

    deploy_dotfile()を実装

--- deploy.sh
+++ deploy.sh
@@ -15,13 +15,39 @@ dotfiles=(
 test ! -d ${backup_dir} && mkdir ${backup_dir}
 test ! -d ${config_dir} && mkdir ${config_dir}
 
-for file in "${dotfiles[@]}"
-do
-    # dotfileが存在する場合はバックアップ
-    if [ -f "${file}" ]; then
-        mv ${file} ${backup_dir}
+function deploy_dotfile() {
+    if [ "$#" -lt 1 ]; then
+        echo 'Argument is missing.' 2>&1
+        exit 1
     fi
 
-  # シンボリックリンク作成
-  ln -s ${work_dir}/$(basename ${file}) ${file}
+    local file_path=${1}
+
+    # 配置先にシンボリックリンクがすでに存在する場合はスキップ
+    if [ -L "${file_path}" ]; then
+        echo "Skip symlink creation. [${file_path}]"
+        return
+    fi
+
+    # 配置先にファイルが存在する場合はバックアップ
+    if [ -f "${file_path}" ]; then
+        echo "Backup dotfile. [${file_path}]"
+        mv ${file_path} ${backup_dir}
+    fi
+
+    # 配置先ディレクトが存在しない場合は作成
+    local dst_dir=$(dirname file_path)
+    if [ ! -d "${dst_dir}" ]; then
+        echo "create directory. [${dst_dir}]"
+        mkdir -p ${dst_dir}
+    fi
+
+    # シンボリックリンク作成
+    echo "Create symlink. [${file_path}]"
+    ln -s ${work_dir}/$(basename ${file_path}) ${file_path}
+}
+
+for file in "${dotfiles[@]}"
+do
+    deploy_dotfile $file
 done
