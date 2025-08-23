79879579df267c578b9d576843aa6e960bdc98ad
hotaru <36354847+hotaru51@users.noreply.github.com>
2025-08-23 13:38:44 +0000

    feat: ファイル配置場所リストを参照してデプロイを行うようにスクリプトを修正

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,10 @@
 
 set -ue
 
-work_dir=$(cd $(dirname $0); pwd)
-backup_dir=${work_dir}/old
-config_dir=${HOME}/.config
-
-dotfiles=(
-    ${HOME}/.zprofile
-    ${HOME}/.zshrc
-    ${config_dir}/starship.toml
-)
+work_dir="$(cd $(dirname $0) && pwd)"
+backup_dir="${work_dir}/old"
 
 test ! -d ${backup_dir} && mkdir ${backup_dir}
-test ! -d ${config_dir} && mkdir ${config_dir}
 
 function deploy_dotfile() {
     if [ "$#" -lt 1 ]; then
@@ -21,33 +13,46 @@ function deploy_dotfile() {
         exit 1
     fi
 
-    local file_path=${1}
+    local src_file="$(echo "${1}" | cut -d ':' -f 1)"
+    local dst_file="$(echo "${1}" | cut -d ':' -f 2)"
 
-    # 配置先にシンボリックリンクがすでに存在する場合はスキップ
-    if [ -L "${file_path}" ]; then
-        echo "Skip symlink creation. [${file_path}]"
+    local src_path="${work_dir}/${src_file}"
+    local dst_path="${HOME}/${dst_file}"
+
+    # src_pathが存在しない場合はスキップ
+    if [ ! -f "${src_path}" ] && [ ! -d "${src_path}" ]; then
+        echo "source file not found. [${src_path}]"
         return
     fi
 
-    # 配置先にファイルが存在する場合はバックアップ
-    if [ -f "${file_path}" ]; then
-        echo "Backup dotfile. [${file_path}]"
-        mv ${file_path} ${backup_dir}
+    # 配置先にシンボリックリンクがすでに存在する場合
+    if [ -L "${dst_path}" ]; then
+        # シンボリックリンクのリンク先が元ファイルと同じならスキップ
+        if [ "$(realpath "${dst_path}")" = "${src_path}" ]; then
+            echo "Skip symlink creation. [${dst_path}]"
+            return
+        fi
+    fi
+
+    # 配置先にファイル or ディレクトリが存在する場合はバックアップ
+    if [ -f "${dst_path}" ] || [ -d "${dst_path}" ]; then
+        echo "Backup dotfile. [${dst_path}]"
+        mv ${dst_path} ${backup_dir}
     fi
 
     # 配置先ディレクトが存在しない場合は作成
-    local dst_dir=$(dirname file_path)
+    local dst_dir=$(dirname ${dst_path})
     if [ ! -d "${dst_dir}" ]; then
         echo "create directory. [${dst_dir}]"
         mkdir -p ${dst_dir}
     fi
 
     # シンボリックリンク作成
-    echo "Create symlink. [${file_path}]"
-    ln -s ${work_dir}/$(basename ${file_path}) ${file_path}
+    echo "Create symlink. [${src_path} -> ${dst_path}]"
+    ln -sf ${src_path} ${dst_path}
 }
 
-for file in "${dotfiles[@]}"
+while read line
 do
-    deploy_dotfile $file
-done
+    deploy_dotfile "${line}"
+done < ${work_dir}/loc_list.txt
